6fca36ca793b882741457c83c52e048c6f54eb4c
GabrielSilva88 <gabrielmg.silva@outlook.com>
2024-01-26 09:24:09 -0300

    Atualização Seção-9: Aplicando Styled Components

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 // biblioteca do css "npm install styled-components"
 
 import React from 'react';
-import {Container, Titulo} from './src/style';
+import { Container, Titulo, BotaoPrincipal, BotaoTexto, Nome } from './src/style';
 
 import styled from 'styled-components/native';
 
 export default function App() {
   return (
     <Container>
-      <Titulo>Styled Componentes: CSS para java script</Titulo>
+      <Titulo cor="#FF0000">Styled Componentes: CSS para java script</Titulo>
+      <Nome>Olá Mundo, por Gabriel!</Nome>
+      <BotaoPrincipal onPress={ () => alert('CLICOU!')}>
+        <BotaoTexto>Entrar</BotaoTexto>
+      </BotaoPrincipal>
     </Container>
   );
 }
--- src/style.js
+++ src/style.js
@@ -1,15 +1,35 @@
 // sempre inportar biblioteca import styled from 'styled-components/native';
 import styled from 'styled-components/native';
 
-// -- `` -- não utilizar "" ou ''
+// UTILIZAR  -- `` -- 
+//não utilizar "" ou ''
 export const Container = styled.View`
 flex: 1;
 justify-content: center;
 align-items: center;
-
+background-color: #121212;
 `;
 
 export const Titulo = styled.Text`
 color: #FFF;
 font-size: 25px;
+`;
+
+export const Nome = styled.Text`
+color: #FFF;
+font-size: 25px;
+`;
+
+export const BotaoPrincipal = styled.TouchableOpacity`
+background-color: #DDD;
+padding: 5px;
+border-radius: 5px;
+width: 90%;
+justify-content: center;
+align-items: center;
+`;
+
+export const BotaoTexto = styled.Text`
+color: #000;
+font-size: 20px;
 `;
